01b1ce6305af200d1c6752c399568ee91186ef36
AdrianusVieira <adrianusvieira@gmail.com>
2026-05-24 11:32:24 -0300

    feat: update database configuration and .gitignore

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,10 @@
+import os
 from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///billboard.db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
